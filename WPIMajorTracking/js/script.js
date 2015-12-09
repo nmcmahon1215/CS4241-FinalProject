987cd839d314f91211c0fd9621621257f77c114a
Huyen Nguyen <nbthanhhuyen@gmail.com>
2015-12-08 19:08:46 -0500

    add folder js + remove js from main.html + registration works perfectly now

---
+++ WPIMajorTracking/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $('#registration-form').validate({ // initialize the plugin
+    rules: {
+      email: {
+        required: true,
+        email: true 
+      },
+      password: {
+        required: true,
+        minlength: 5
+      },
+      passwordRepeat: {
+        required: true,
+        minlength: 5,
+        equalTo: "#password"
+      },
+      firstName: {
+        required: true
+      },
+      lastName: {
+        required: true
+      }
+    },
+    messages: {
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      passwordRepeat: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long",
+        equalTo: "Please enter the same password as above"
+      }
+    },
+    submitHandler: function (form) { // for demo
+       //need to use a post request to handle this
+       alert("Registration form submitted!");
+     }
+   });
+});
