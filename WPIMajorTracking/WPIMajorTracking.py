4b9e37f60ec0096c46b7b26c03cccd22fdab2ff7
Nick McMahon <npm.mcmahon@gmail.com>
2015-12-11 21:19:15 -0500

    Correctly redirects the user from the home page if they are logged in.

--- WPIMajorTracking/WPIMajorTracking.py
+++ WPIMajorTracking/WPIMajorTracking.py
@@ -114,6 +114,10 @@ class BaseHandler(webapp2.RequestHandler):
 class MainHandler(BaseHandler):
   def get(self, *args, **kwargs):
     filePath = self.request.path[8:]
+
+    if self.user is not None:
+        self.redirect("/secured")
+
     if filePath == "":
         filePath = "home.html"
 
@@ -325,7 +329,7 @@ class SecuredHandler(BaseHandler):
   def get(self, *args, **kwargs):
     filePath = self.request.path[9:]
     if filePath == "":
-        filePath = "home.html"
+        filePath = "userHome.html"
 
     self.render_template(filePath, { 'directory': 'secured' })
 
