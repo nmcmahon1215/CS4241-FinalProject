7844c3cfc05353bb425eca712c5f29270c66f907
Sam La <sla@wpi.edu>
2015-12-12 21:46:00 -0500

    Adds resend verification email for new users

--- WPIMajorTracking/SignupHandler.py
+++ WPIMajorTracking/SignupHandler.py
@@ -12,33 +12,30 @@ class SignupHandler(BaseHandler.BaseHandler):
     password = self.request.get('password')
     last_name = self.request.get('lastname')
     major = self.request.get_all('major')
-    # resendEmail = self.request.get('resendEmail')
-    # resendVerification = self.request.get('resendVerification')
-    # logging.info("re: %s, rv: %s", resendEmail, resendVerification)
+    resendEmail = self.request.get('resendEmail')
+    resendVerification = self.request.get('resendVerification')
 
-    # if not resendEmail:
-    unique_properties = None
-    user_data = self.user_model.create_user(email,
-      unique_properties,
-      email_address=email, name=name, password_raw=password,
-      last_name=last_name, major=major[0], verified=False)
+    if not resendEmail:
+      unique_properties = None
+      user_data = self.user_model.create_user(email,
+        unique_properties,
+        email_address=email, name=name, password_raw=password,
+        last_name=last_name, major=major[0], verified=False)
 
-    if not user_data[0]: #user_data is a tuple
-      self._serve_page(duplicate=True, duplicateKeys=user_data[1])
-      return
+      if not user_data[0]: #user_data is a tuple
+        self._serve_page(duplicate=True, duplicateKeys=user_data[1])
+        return
 
-    user = user_data[1]
-    user_id = user.get_id()
+      user = user_data[1]
+      user_id = user.get_id()
 
-    token = self.user_model.create_signup_token(user_id)
+      token = self.user_model.create_signup_token(user_id)
 
-    # if resendVerification:
-    # logging.info("Resent Email for %s", email)
-    # verification_url = resendVerification
+      verification_url = self.uri_for('verification', type='v', user_id=user_id,
+        signup_token=token, _full=True)
 
-
-    verification_url = self.uri_for('verification', type='v', user_id=user_id,
-      signup_token=token, _full=True)
+    if resendVerification:
+      verification_url = resendVerification
 
     receiverString = name + " " + last_name + "<" + email + ">";
     email_body = """Hello new user,\n\nPlease verify your account by going to this link: """ + verification_url;
@@ -49,15 +46,16 @@ class SignupHandler(BaseHandler.BaseHandler):
     message.body = email_body
     message.send()
 
-    self._serve_page(email_sent=True, verification_url=verification_url)
+    self._serve_page(email_sent=True, verification_url=verification_url, resendEmail=resendEmail)
 
-  def _serve_page(self, email_sent=False, verification_url='', duplicate=False, duplicateKeys=''):
+  def _serve_page(self, email_sent=False, verification_url='', duplicate=False, duplicateKeys='', resendEmail=''):
     email_address = self.request.get('email_address')
     params = {
       'email_address': email_address,
       'email_sent': email_sent,
       'verification_url': verification_url,
       'duplicate': duplicate,
-      'duplicateKeys': duplicateKeys
+      'duplicateKeys': duplicateKeys,
+      'resendEmail': resendEmail
     }
     self.render_template('signup.html', params)
