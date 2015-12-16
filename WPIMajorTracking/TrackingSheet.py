955f22df2316540d4970db5e61248f31e3d98feb
Nick McMahon <npm.mcmahon@gmail.com>
2015-12-16 17:52:14 -0500

    Changes tracking sheet to use dateTime, and orders those results

--- WPIMajorTracking/TrackingSheet.py
+++ WPIMajorTracking/TrackingSheet.py
@@ -4,10 +4,10 @@ from google.appengine.ext import db
 class TrackingSheet(db.Model):
     email = db.StringProperty(required=True)
     name = db.StringProperty(required=True)
-    date = db.DateProperty(required=True)
+    date = db.DateTimeProperty(required=True)
 
 def get_sheets_by_email(email):
-    q = db.GqlQuery("SELECT * FROM TrackingSheet WHERE email = '" + email + "'")
+    q = db.GqlQuery("SELECT * FROM TrackingSheet WHERE email = '" + email + "' ORDER BY date DESC")
 
     result = []
 
